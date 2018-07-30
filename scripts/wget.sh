14da0c5e5f2f78438ac82b85d69e3839a4df4164
Yimeng Chen <chenyimeng@jd.com>
2018-07-30 23:13:50 +0800

    add docker etcd roles

--- scripts/wget.sh
+++ scripts/wget.sh
@@ -1,4 +1,5 @@
-wget -O cfssl https://pkg.cfssl.org/R1.2/cfssl_linux-amd64
-wget -O cfssljson https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
-wget -O cfssl-certinfo https://pkg.cfssl.org/R1.2/cfssl-certinfo_linux-amd64
-#chmod +x bin/*
+BIN_PATH=../packages
+wget -O $BIN_PATH/cfssl https://pkg.cfssl.org/R1.2/cfssl_linux-amd64
+wget -O $BIN_PATH/cfssljson https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
+wget -O $BIN_PATH/cfssl-certinfo https://pkg.cfssl.org/R1.2/cfssl-certinfo_linux-amd64
+chmod +x $BIN_PATH/*
