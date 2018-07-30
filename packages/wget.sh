d1137f3ba9c17e0d01d3e5e4431ecd363c01293a
Yimeng Chen <chenyimeng@jd.com>
2018-07-30 21:57:39 +0800

    package use lfs

---
+++ packages/wget.sh
@@ -0,0 +1,4 @@
+wget -O cfssl https://pkg.cfssl.org/R1.2/cfssl_linux-amd64
+wget -O cfssljson https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
+wget -O cfssl-certinfo https://pkg.cfssl.org/R1.2/cfssl-certinfo_linux-amd64
+#chmod +x bin/*
